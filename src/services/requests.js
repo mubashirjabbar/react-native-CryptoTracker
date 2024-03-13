c925f1c5c987781e8524b5e2c2bb4951a1c1d2b5
Mubashir <app@Mubashir-Macbook-Pro.local>
2024-03-13 12:53:50 +0500

    finalize the component folder

--- src/services/requests.js
+++ src/services/requests.js
@@ -8,4 +8,20 @@ export const getDetailedCoinData = async (coinId) => {
     console.log(e);
   }
 }
-
+export const getCoinMarketChart = async (coinId, selectedRange) => {
+    try {
+      const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${selectedRange}&interval=hourly`)
+      return response.data;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  export const getMarketData = async (pageNumber = 1) => {
+    try {
+      const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false&price_change_percentage=24h`)
+      return response.data;
+    } catch (e) {
+      console.log(e)
+    }
+  }
