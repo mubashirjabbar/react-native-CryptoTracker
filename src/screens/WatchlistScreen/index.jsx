28b1e8167f9cd3bfc27a5cebed1b69e188ac36bd
Mubashir <app@Mubashir-Macbook-Pro.local>
2024-04-30 12:00:32 +0500

    worked on watchlist screen

--- src/screens/WatchlistScreen/index.jsx
+++ src/screens/WatchlistScreen/index.jsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, RefreshControl } from 'react-native';
+import { useWatchlist } from '../../Contexts/WatchlistContext';
+import CoinItem from '../../components/CoinItem';
+import { getWatchlistedCoins } from '../../services/requests';
 
-const index = () => {
+const WatchlistScreen = () => {
+  const {watchlistCoinIds} = useWatchlist();
+
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const transformCoinIds = () => watchlistCoinIds.join('%2C');
+
+  const fetchWatchlistedCoins = async () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const watchlistedCoinsData = await getWatchlistedCoins(1, transformCoinIds());
+    setCoins(watchlistedCoinsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (watchlistCoinIds.length > 0) {
+      fetchWatchlistedCoins();
+    }
+  }, [watchlistCoinIds]);
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <FlatList 
+      data={coins}
+      renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      refreshControl={
+        <RefreshControl 
+          refreshing={loading}
+          tintColor="white"
+          onRefresh={watchlistCoinIds.length > 0 ? fetchWatchlistedCoins : null}
+        />
+      }
+    />
   )
-}
+};
 
-export default index
+export default WatchlistScreen;
